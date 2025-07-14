3f3e6b3ae12f047d5dfa3d82ef7d1e75dc913aee
bathulamanu <bathulamanohar25@gmail.com>
2025-07-14 23:53:59 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,10 +5,11 @@ cd /home/ubuntu/codedeploy-ttt
 # Login to ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 403135539729.dkr.ecr.us-east-1.amazonaws.com
 
-# Pull latest images
-docker pull 403135539729.dkr.ecr.us-east-1.amazonaws.com/ttt:latest
-docker pull 403135539729.dkr.ecr.us-east-1.amazonaws.com/cicd:latest
+# Pull the correct images
+docker pull 403135539729.dkr.ecr.us-east-1.amazonaws.com/test:latest    # Frontend
+docker pull 403135539729.dkr.ecr.us-east-1.amazonaws.com/cicd:latest    # Backend
 
 # Run containers
 sudo docker compose down || true
 sudo docker compose up -d
+
