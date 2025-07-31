d05cb6f7461bfd87c06b41fee309da7067d0f124
bathulamanu <bathulamanohar25@gmail.com>
2025-08-01 00:34:22 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,6 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 docker pull403135539729.dkr.ecr.us-east-1.amazonaws.com/ttt:latest
 docker pull 403135539729.dkr.ecr.us-east-1.amazonaws.com/cicd:latest
 
-# Run containers
+# Run containers 
 sudo docker compose down || true
 sudo docker compose up -d
